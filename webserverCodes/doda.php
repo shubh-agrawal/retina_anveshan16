ae8ab920bca6f68716a19c3737b5494b6ab2e67e
Aashay-Bhise <meetaashay3@gmail.com>
2016-10-12 05:34:58 +0530

    Added webserver php code not tested yet

---
+++ webserverCodes/doda.php
@@ -0,0 +1,49 @@
+ <?php
+$dbhost = 'localhost';
+$dbuser = 'shubhagr_retina';
+$dbpass = 'anveshan_doda';
+$conn=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+
+
+
+if(isset($_GET['user_cur_lat'])&&isset($_GET['user_cur_long'])&&isset($_GET['gesture'])&&isset($_GET['user_tar_lat'])&&isset($_GET['user_tar_long'])){
+                        
+                      
+if(!$conn){
+            	      
+die(mysql_error());
+
+}
+else{
+$CLat = $_GET['user_cur_lat'];
+$CLong = $_GET['user_cur_long'];
+$gesture= $_GET['gesture'];
+$TLat = $_GET['user_tar_lat'];
+$TLong = $_GET['user_tar_long'];
+
+              	 
+$sql = "INSERT INTO dodalogtable (targetLati, targetLongi, currentLati, currentLongi, gesture) VALUES ($TLat, $TLong, $CLat, $CLong, $gesture)";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+    echo "Target Latitude : " . $TLat . "  Target Longitude : " . $TLong . "  Current Latitude : " . $CLat . "  Current Longitude : " . $CLong . "  Gesture : " . $gesture . " | ";
+} else {
+    echo "Maiyya Chud Gayi : " . $sql . "<br>" . mysqli_error($conn);
+}
+
+
+}
+else 
+{
+
+}
+
+
+else
+{
+    echo " Badi Wali Chudi Hai | Dhang se daalo request" ;
+}
+mysqli_close($conn);
+
+?>
+
