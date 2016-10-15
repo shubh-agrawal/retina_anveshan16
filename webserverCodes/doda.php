d0232500e016391718c26f5fbef4d4b2e4373c96
Aashay-Bhise <meetaashay3@gmail.com>
2016-10-15 11:47:40 +0530

    made minor changes, sql database up and running

--- webserverCodes/doda.php
+++ webserverCodes/doda.php
@@ -1,20 +1,22 @@
  <?php
+
 $dbhost = 'localhost';
 $dbuser = 'shubhagr_retina';
 $dbpass = 'anveshan_doda';
+$dbname = 'shubhagr_dodadb';
 $conn=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
-
-
-
-if(isset($_GET['user_cur_lat'])&&isset($_GET['user_cur_long'])&&isset($_GET['gesture'])&&isset($_GET['user_tar_lat'])&&isset($_GET['user_tar_long'])){
-                        
                       
 if(!$conn){
             	      
 die(mysql_error());
 
 }
-else{
+
+
+
+if(isset($_GET['user_cur_lat'])&&isset($_GET['user_cur_long'])&&isset($_GET['gesture'])&&isset($_GET['user_tar_lat'])&&isset($_GET['user_tar_long'])){
+                                    
+
 $CLat = $_GET['user_cur_lat'];
 $CLong = $_GET['user_cur_long'];
 $gesture= $_GET['gesture'];
@@ -33,17 +35,11 @@ if (mysqli_query($conn, $sql)) {
 
 
 }
-else 
-{
-
-}
-
-
 else
 {
     echo " Badi Wali Chudi Hai | Dhang se daalo request" ;
 }
 mysqli_close($conn);
-
+			
 ?>
 
