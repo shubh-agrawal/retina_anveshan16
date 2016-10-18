2d173717bb45e353b21c0b5bdfbe08c0bc012d5b
Aashay-Bhise <meetaashay3@gmail.com>
2016-10-19 03:09:40 +0530

    Added Google Maps Api with a new API_KEY.

--- webserverCodes/doda.php
+++ webserverCodes/doda.php
@@ -1,5 +1,6 @@
- <?php
+<?php
 
+global $CLat; 
 $dbhost = 'localhost';
 $dbuser = 'shubhagr_retina';
 $dbpass = 'anveshan_doda';
@@ -42,4 +43,64 @@ else
 mysqli_close($conn);
 			
 ?>
+
+<html>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCDSBgT0i8uM-pk8J62gon4jwkHpn14dT0&callback=initMap" type="text/javascript"></script>
+
+<script type="text/javascript">
+
+var customIcons = {
+      restaurant: {
+        icon: 'http://labs.google.com/ridefinder/images/mm_20_blue.png'
+      },
+      bar: {
+        icon: 'http://labs.google.com/ridefinder/images/mm_20_red.png'
+      }
+    };
+
+
+
+function myMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+        center: new google.maps.LatLng(22.316273, 87.306028),
+        zoom: 13,
+        mapTypeId: 'roadmap'
+      });
+  var point = new google.maps.LatLng(
+              parseFloat($CLat),
+              parseFloat($CLong));
+
+  var icon = customIcons[restaurant] || {};
+
+  var marker = new google.maps.Marker({
+            map: map,
+            position: point,
+            icon: icon.icon
+          });
+  
+  var icon = customIcons[bar] || {};
+  
+  var point = new google.maps.LatLng(
+              parseFloat($TLat),
+              parseFloat($TLong));
+
+  var marker = new google.maps.Marker({
+            map: map,
+            position: point,
+            icon: icon.icon
+          }); 
+
+
+}
+</script>
+
+<body onload="myMap()">
+
+<h1>            Retina Squared : Google Maps</h1>
+
+<div id="map" style="width:100%;height:500px"></div>
+
+
+</body>
+</html>
 
