7fe1abfdcd3214f21e5135a30d9e247d1445d657
Shubh Agrawal <shubh.agrawal111@gmail.com>
2017-01-21 15:33:17 +0530

    added origin lati and longi

--- src/webserver_test/src/webserver.py
+++ src/webserver_test/src/webserver.py
@@ -11,11 +11,13 @@ prev_coord = []
 def targetcallback(data):
     #rospy.loginfo( "Target Heading : " + str(data.target_heading) + " | " )
     global prev_coord
-    current_coord = parse_coord(data.current_address)
-    #start_coord   = parse_coord(data.start_coord)
-    #destination_coord = parse_coord(data.destination_coord)
     start_coord = [" " , " "]
-    destination_coord = [" " ," "]
+    destination_coord = [" " , " "]
+    current_coord = parse_coord(data.current_address)
+    start_coord   = parse_coord(data.start_crdnts)
+    destination_coord = parse_coord(data.target_crdnts)
+    #start_coord = [" " , " "]
+    #destination_coord = [" " ," "]
     #lati%longi@
     prev_coord = current_coord
     package = start_coord + current_coord + destination_coord
@@ -33,8 +35,8 @@ def parse_coord(gps_str):
     return coord
 
 def gps_distance(coord_0,coord_1):
-	d2r = math.pi/180.0
-	dlong = (coord_1[1] - coord_0[1]) * d2r
+    d2r = math.pi/180.0
+    dlong = (coord_1[1] - coord_0[1]) * d2r
     dlat = (coord_1[0] - coord_0[0]) * d2r
     a = pow(math.sin(dlat/2.0), 2) + math.cos(coord_0[0]*d2r) * math.cos(coord_1[0]*d2r) * pow(math.sin(dlong/2.0), 2)
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
@@ -42,16 +44,15 @@ def gps_distance(coord_0,coord_1):
     return distance
 
 def sendreq(package):
-	url = 'http://shubhagrawal.in/dodanew.php?'
-	rq  = ""
-	if (package[0] == " " or package[1] == " " or package[4] == " " or package[5] == " "):
-		rq = url + 'user_cur_lat=' + package[2] + '&user_cur_long=' + package[3] + '&gesture=3&user_tar_lat=55.674983629&user_tar_long=23.423429'
+    url = 'http://shubhagrawal.in/dodanew.php?'
+    rq  = ""
+    if (package[0] == " " or package[1] == " " or package[4] == " " or package[5] == " "):
+        rq = url + 'user_cur_lat=' + package[2] + '&user_cur_long=' + package[3] + '&gesture=3&user_tar_lat=55.674983629&user_tar_long=23.423429'
     else:
-    	rq = url + 'user_cur_lat=' + package[2] + '&user_cur_long=' + package[3] + '&gesture=3' + '&user_tar_lat=' + package[4] + '&user_tar_long=' + package[5]
-
+        rq = url + 'user_cur_lat=' + package[2] + '&user_cur_long=' + package[3] + '&gesture=3' + '&user_tar_lat=' + package[4] + '&user_tar_long=' + package[5]
     req = urllib2.Request(rq)
-	response = urllib2.urlopen(req)
-	rq = ""
+    response = urllib2.urlopen(req)
+    rq = ""
 
 
 def listener():
@@ -60,7 +61,7 @@ def listener():
 
 
 if __name__ == '__main__':
-#    listener()
+    listener()
     print "webdoda"
     try:
         rate = rospy.Rate(100)
